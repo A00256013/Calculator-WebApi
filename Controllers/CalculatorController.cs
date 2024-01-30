fdb27f54c5749b076ea47496fbf8383b4f220a27
A00256013 <a00256013@mycambrian.ca>
2024-01-30 16:37:47 -0500

    Created methods for subtract, multiply, divide, and modulo

--- Controllers/CalculatorController.cs
+++ Controllers/CalculatorController.cs
@@ -15,5 +15,51 @@ namespace Calculator.Controllers
             return Ok(sum);
         }
 
+        [HttpGet]
+        [Route("Subtract")]
+        public IActionResult Subtract(decimal num1, decimal num2)
+        {
+            decimal difference = num1 - num2;
+            return Ok(difference);
+        }
+
+        [HttpGet]
+        [Route("Multiply")]
+        public IActionResult Multiply(decimal num1, decimal num2)
+        {
+            decimal product = num1 * num2;
+            return Ok(product);
+        }
+
+        [HttpGet]
+        [Route("Divide")]
+        public IActionResult Divide(decimal num1, decimal num2)
+        {
+            if (num2 != 0)
+            {
+                decimal quotient = num1 / num2;
+                return Ok(quotient);
+            }
+            else
+            {
+                return BadRequest("Cannot divide by zero.");
+            }
+        }
+
+        [HttpGet]
+        [Route("Modulo")]
+        public IActionResult Modulo(decimal num1, decimal num2)
+        {
+            if (num2 != 0)
+            {
+                decimal remainder = num1 % num2;
+                return Ok(remainder);
+            }
+            else
+            {
+                return BadRequest("Cannot calculate modulo with zero divisor.");
+            }
+        }
+
     }
 }
