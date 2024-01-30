8719d0a2adb4ac8e5602e1441ce7b6179b279d73
A00256013 <a00256013@mycambrian.ca>
2024-01-30 15:40:21 -0500

    Created method for Add

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Add")]
+        public IActionResult Add(decimal num1, decimal num2)
+        {
+            decimal sum = num1 + num2;
+            return Ok(sum);
+        }
+
+    }
+}
